ae0d9972c841ded2bb639da64c733ccf39ca48ed
Normunds Pureklis <norchahack@gmail.com>
2025-09-10 20:17:51 +0200

    Fix package setup Python version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: >=2.7",
+        "Programming Language :: Python :: 2.7",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
